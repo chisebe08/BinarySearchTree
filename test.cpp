6311ea85cab769c5f995bc6d7d80ffe541580e11
chisebe08 <georgechisebe08@gmail.com>
2024-12-29 19:23:28 +0300

    New change on countleaves added

--- test.cpp
+++ test.cpp
@@ -23,6 +23,27 @@ void testDelete() {
     assert(tree.search(5) == true);
     assert(tree.search(15) == true);
 }
+// test.cpp (additional code)
+void testFindMinMaxHeight() {
+    BST tree;
+    tree.insert(10);
+    tree.insert(5);
+    tree.insert(15);
+    tree.insert(3);
+    tree.insert(7);
+
+    assert(tree.findMin()->data == 3);
+    assert(tree.findMax()->data == 15);
+    assert(tree.height() == 2);
+}
+
+int main() {
+    testInsertAndSearch();
+    testDelete();
+    testFindMinMaxHeight();
+    std::cout << "All tests passed!" << std::endl;
+    return 0;
+}
 
 int main() {
     testInsertAndSearch();
