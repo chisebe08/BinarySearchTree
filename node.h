a6668cff0c6ab5d3e3b06b3e31ad6b0bc537357d
chisebe08 <georgechisebe08@gmail.com>
2024-12-29 18:08:25 +0300

    Define Node structure with constructor

---
+++ node.h
@@ -0,0 +1,17 @@
+#ifdef NODE_H
+#define NODE_H
+ 
+ class Node {
+public:
+     int data;
+     Node* left;
+     Node* right;
+
+     Node(int val) {
+        data = val;
+        left = nullptr;
+        right = nullptr;
+       }
+     };
+     
+ }
